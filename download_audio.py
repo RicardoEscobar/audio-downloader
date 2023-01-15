dd1ff166ed86e1d494b1942d8cca004d547ac888
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-01-15 15:49:08 -0600

    Add callback functions and uses a multithreads download_audio.py

--- download_audio.py
+++ download_audio.py
@@ -1,28 +1,37 @@
 from pathlib import Path
+import threading
 import pytube
 
 
-def callback(output_file_path: Path) -> str:
+def on_complete_callback(stream, file_path) -> None:
     """Callback function prints message"""
-    return f'Audio has been downloaded to: {output_file_path}'
+    print(f'Audio stream: {stream}\nhas been downloaded to: {file_path}')
 
 
-def download_audio(url: str, callback) -> Path:
-    youtube = pytube.YouTube(url)
+def on_progress_callback(stream, chunk, bytes_remaining) -> None:
+    """Callback function when there is progress"""
+    total_bytes = stream.filesize
+    completed_bytes = total_bytes - bytes_remaining
+    percentage_completed = (completed_bytes / total_bytes) * 100
+
+    print(f"""Progress = {percentage_completed:.2f}%""")
+
+
+def download_audio(url: str, on_complete_callback, on_progress_callback) -> Path:
+    youtube = pytube.YouTube(
+        url,
+        on_complete_callback=on_complete_callback,
+        on_progress_callback=on_progress_callback)
 
     audio_stream = youtube.streams.filter(
         only_audio=True).order_by('abr').desc().first()
-    print(f'audio_stream = {audio_stream}')
-    audio_download_location = audio_stream.download()
 
-    output_file_path = Path(audio_download_location)
-
-    # Callback
-    print(callback(output_file_path))
-
-    return output_file_path
+    # Download the audio file
+    download_thread = threading.Thread(target=audio_stream.download)
+    download_thread.start()
+    print('download_thread started!!!')
 
 
 if __name__ == '__main__':
-    video_url = "https://youtu.be/qpCNaRkIh2E"
-    download_audio(video_url, callback)
+    video_url = "https://youtu.be/d0xeShmVx1Q"
+    download_audio(video_url, on_complete_callback, on_progress_callback)
