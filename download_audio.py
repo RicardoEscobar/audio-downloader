17e1d258332ca345e1ee477b16fa451d5212f517
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-01-15 20:18:25 -0600

    Add Call the convert_audio_format function

--- download_audio.py
+++ download_audio.py
@@ -1,11 +1,34 @@
 from pathlib import Path
 import threading
 import pytube
+from pydub import AudioSegment
+
+
+def extract_bitrate(bitrate: str) -> str:
+    return bitrate.rstrip('bps')
+
+
+def convert_audio_format(input_file: str, output_file: str, abr: str):
+    sound = AudioSegment.from_file(input_file, format="webm")
+    print(f'abr = {abr}')
+
+    # Use export() method to convert the file to MP3 format
+    convert_thread = threading.Thread(
+        target=sound.export, args=(output_file,), kwargs={"format": "mp3", "bitrate": abr})
+    convert_thread.start()
+
+    print('convert_thread started!!!')
 
 
 def on_complete_callback(stream, file_path) -> None:
     """Callback function prints message"""
     print(f'Audio stream: {stream}\nhas been downloaded to: {file_path}')
+
+    # Call the convert_audio_format function
+    input_file_path = Path(file_path)
+    output_file_path = input_file_path.with_suffix('.mp3')
+    audio_bitrate = extract_bitrate(stream.abr)
+    convert_audio_format(input_file_path, output_file_path, audio_bitrate)
 
 
 def on_progress_callback(stream, chunk, bytes_remaining) -> None:
